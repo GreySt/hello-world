4a901ef89e6be507863b6d0da50faa51311a3eab
GreySt <trtech21@googlemail.com>
2018-06-04 15:53:38 +0200

    Added my first project and commited it

---
+++ FirstProject/FirstProject.cpp
@@ -0,0 +1,20 @@
+/*
+* FirstProject.cpp
+* Created by: rost
+* 04.06.2018
+* 15:46:38
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+//Todo Place code here
+cout << "Eclipse C++" << endl;
+
+return 0;
+
+}
+
